b676913e402107e5418c1f1c5e2418f23b650878
YR988 <arseniysavinov@gmail.com>
2024-12-14 15:57:10 +0300

    upgrade

--- projects/work/main.go
+++ projects/work/main.go
@@ -1,16 +1,19 @@
-package main
-
-import (
-	"fmt"
-	"time"
-	// "sync"
-)
-
-func work() {
-	time.Sleep(time.Millisecond * 50)
-	fmt.Println("done")
-}
-
-func main() {
-	// необходимо в отдельных горутинах вызвать функцию work() 10 раз и дождаться результатов выполнения вызванных функций
-}
+package main 
+import ( 
+"fmt" 
+"sync" 
+) 
+func work() { 
+fmt.Println("Hello, World!") 
+} 
+func main() { 
+wg := sync.WaitGroup{} 
+wg.Add(10) 
+for i := 0; i < 10; i++ { 
+go func() { 
+work() 
+wg.Done() 
+}() 
+} 
+wg.Wait() 
+} 
